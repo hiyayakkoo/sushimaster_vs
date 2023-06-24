83592464f14c79c51a79270a052116794286e18c
ten3 <ken.osada@dena.com>
2023-06-25 00:41:20 +0900

    connect wallet!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import "./App.css";
 
 const App = () => {
   const [userChoice, setUserChoice] = useState(null);
   const [cpuChoice, setCpuChoice] = useState(null);
   const [result, setResult] = useState("");
+  const [connected, setConnected] = useState(false);
+  const [accounts, setAccounts] = useState([]);
 
   const choices = ["グー", "チョキ", "パー"];
+
+  useEffect(() => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert("Ethereum ブラウザのインストールを検討してください。MetaMask を試してください!");
+    }
+  }, []);
 
   const handleClick = (choice) => {
     setUserChoice(choice);
@@ -30,9 +43,22 @@ const App = () => {
     }
   };
 
+  const connectWallet = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccounts(accounts);
+    setConnected(true);
+  };
+
   return (
       <div className="App">
         <h1>じゃんけんゲーム</h1>
+          {connected ? (
+              <p>接続されたアカウント: {accounts[0]}</p>
+          ) : (
+              <button onClick={connectWallet}>ウォレットに接続</button>
+          )}
         <div>
           {choices.map((choice) => (
               <button key={choice} onClick={() => handleClick(choice)}>
